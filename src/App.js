db12b20eca55f255ee875e350a12bbb82fbd2179
Hasami <hasami110065518@gapp.nthu.edu.tw>
2022-10-12 18:16:48 +0800

    addblock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,238 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import {dia, shapes, } from 'jointjs';
+
+
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sysmaker</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">File</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">View</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Edit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Setting</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
+}
+
+function Taglib(){
+  return (
+    <div class='lib'>taglib</div>
+  )
+}
+
+class ContextMenu extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.addBlock = props.actions
+    console.log(props.actions)
+
+    this.state ={
+      showMenu: false,
+      xPos: 0,
+      yPos: 0,
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("click", this.handleClick)
+    document.addEventListener("contextmenu", this.handleContextMenu)
+  }
+
+  handleContextMenu = (event) => {
+    event.preventDefault() // cancel the default context menu
+    // console.log("right click")
+    this.setState({
+      xPos: `${event.pageX}px`,
+      yPos: `${event.pageY}px`,
+      showMenu: true,
+    });
+  }
+
+  handleClick = (event) => {
+    event.preventDefault() // cancel the default context menu
+    this.setState({
+      showMenu: false,
+    });
+  }
+
+  // addBlock() {
+
+  //   var rect = new shapes.standard.Rectangle();
+  //   rect.position(100, 30);
+  //   rect.resize(100, 40);
+  //   rect.attr({
+  //       body: {
+  //           fill: 'blue'
+  //       },
+  //       label: {
+  //           text: 'Hello',
+  //           fill: 'white'
+  //       }
+  //   });
+  //   rect.addTo(this.canvas);
+  // }
+
+  render() {
+    const { showMenu, xPos, yPos } = this.state;
+    // console.log(this.state)
+
+    if (showMenu)
+      return (
+        <div class="btn-group-vertical text-center context-menu" style={{
+          top: yPos,
+          left: xPos,
+        }}>
+            <button type= "button" class="btn btn-light" onClick={(e) => {e.preventDefault();this.addBlock();}} >New Block</button>
+            <button type= "button" class="btn btn-light">Option2</button>
+            <button type= "button" class="btn btn-light">Option3</button>
+        </div>
+      );
+    else return null;
+  }
+}
+
+
+class Canvas extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.addBlock = this.addBlock.bind(this);
+  }
+
+  componentDidMount() {
+
+    var namespace = shapes;
+                
+    var graph = new dia.Graph({}, { cellNamespace: namespace });
+
+    var paper = new dia.Paper({
+      el: document.getElementById("canvas"),
+      model: graph,
+      width: "100%",
+      height: "100%",
+      gridSize: 1,
+      cellViewNamespace: namespace
+
+    });
+
+    this.namespace = namespace
+    this.graph = graph
+    this.paper = paper
+
+    this.setState({
+      graph: "graph"
+    })
+
+    this.demo();
+
+  }
+
+  demo() {
+    // console.log(this.state)
+    var graph = this.graph
+    var rect = new shapes.standard.Rectangle();
+    rect.position(100, 30);
+    rect.resize(100, 40);
+    rect.attr({
+        body: {
+            fill: 'blue'
+        },
+        label: {
+            text: 'Hello',
+            fill: 'white'
+        }
+    });
+    rect.addTo(graph);
+  
+    var rect2 = rect.clone();
+    rect2.translate(300, 0);
+    rect2.attr('label/text', 'World!');
+    rect2.addTo(graph);
+  
+    var link = new shapes.standard.Link();
+    link.source(rect);
+    link.target(rect2);
+    link.addTo(graph);
+  }
+
+  addBlock() {
+
+    var rect = new shapes.standard.Rectangle();
+    rect.position(150, 80);
+    rect.resize(100, 40);
+    rect.attr({
+        body: {
+            fill: 'blue'
+        },
+        label: {
+            text: 'New',
+            fill: 'white'
+        }
+    });
+    rect.addTo(this.graph);
+    console.log("hi")
+  }
+
+
+
+  render() {
+    // console.log(this.state)
+
+    return (
+
+      <div id="canvas">
+        <ContextMenu actions={this.addBlock}></ContextMenu>
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <div class="lib row row-cols-3" >
+        <div class="col-2 lib bg-info" >
+          <Taglib></Taglib>
+        </div>
+        <div class="col-8 lib bg-warning">
+          <Canvas></Canvas>
+        </div>
+        <div class="col-2 lib bg-info">
+          2 of 2
+        </div>
     </div>
+    </React.Fragment>
   );
 }
+
+// export default ContainerExample;
+
 
 export default App;
