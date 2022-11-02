0e088574a67c67b7071a53a69c56bf418e367644
Hasami <hasami110065518@gapp.nthu.edu.tw>
2022-11-02 11:47:41 +0800

    imigrate all color related settings to css

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,24 +1,24 @@
 export function Navbar() {
     return (
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Sysmaker</a>
+      <a class="navbar-brand" href="#"><div>Sysmaker</div></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link " aria-current="page" href="#">File</a>
+            <a class="nav-link " aria-current="page" href="#"><div>File</div></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">View</a>
+            <a class="nav-link" href="#"><div>View</div></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Edit</a>
+            <a class="nav-link" href="#"><div>Edit</div></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link ">Setting</a>
+            <a class="nav-link "><div>Settings</div></a>
           </li>
         </ul>
       </div>
