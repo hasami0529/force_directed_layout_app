1c748cce5b0f94e03737d303dbb88038abc9ec2b
Hasami <hasami110065518@gapp.nthu.edu.tw>
2022-11-22 15:32:39 +0800

    cont.

--- src/component/canvas.js
+++ src/component/canvas.js
@@ -1,43 +1,21 @@
-import { canvasActions, selectCanvas } from '../store/slice/canvas';
-import { useDispatch, useSelector } from 'react-redux'
+import { canvasActions } from '../store/slice/canvas';
+import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-
-
-
-
-
 
 export function Canvas() {
 
     const dispatch = useDispatch()
-    const states = useSelector(selectCanvas)
 
     useEffect(
         () => {
             console.log('init')
-
-
-
             dispatch(
                 canvasActions.initPaper({ dispatch, })
             )
-
-            // dispatch(
-            //     canvasActions.demo()
-            // )
-
         },[]
     )
 
-    // useEffect(
-    //     () => {
-    //         try {
-    //             initPaperEvents(states.paper, dispatch)
-    //         } catch (error) {
-    //             console.warn('Add listeners will fail on the first time. It might be a bug.')
-    //         }
-    //     }, [states.paper]
-    // )
+
 
     return (
         <div class="canvas" id="canvas"></div>
