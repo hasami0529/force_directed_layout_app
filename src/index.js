88f456a1fb032bf9f03089c059427386e0601391
Hasami <hasami110065518@gapp.nthu.edu.tw>
2023-02-13 13:10:52 +0800

    cont.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'react-checkbox-tree/lib/react-checkbox-tree.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'react-checkbox-tree/lib/react-checkbox-tree.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
