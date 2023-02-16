b681dc565812895fcecd63793a1a484ab3e563ae
Hasami <hasami110065518@gapp.nthu.edu.tw>
2023-02-16 17:02:11 +0800

    cont.

--- src/diagramlib/index.js
+++ src/diagramlib/index.js
@@ -1,10 +1,6 @@
 import { createNormalLink } from './shapes/link'
 import { createBlock, createBus } from './shapes/rect'
 import { setLabel } from '../utils'
-import { shapes } from 'jointjs'
-
-import { PAPERHIEGHT, PAPERWIDTH, GRID } from './config'
-import { layout, idealLayout } from './layout'
 
 export { addBlock, setLabel, addPort, addSlot } from './factory'
 export { init, initPaperEvents } from './init'
@@ -22,9 +18,11 @@ function demo_diagram(graph, paper) {
     const dac = createBlockWithParam(paper, graph, 'DAC', ['DAC', 'audio_interface'])
     const speaker = createBlockWithParam(paper, graph, 'speaker', ['speaker', 'output'])
 
-    let blocks = [mcu, dac, speaker]
+    mcu.position(10,20)
+    dac.position(30,40)
+    speaker.position(50,70)
 
-    idealLayout(graph, paper, blocks)
+    let blocks = [mcu, dac, speaker]
 
     // decide size and positon based on tags
 
@@ -32,45 +30,17 @@ function demo_diagram(graph, paper) {
 
     // rendering
     graph.addCells(...blocks)
+
+    return blocks
 }
 
 export function demo(graph, paper) {
 
-    drawSections(graph, paper)
-
-    demo_diagram(graph, paper)
+    // drawSections(graph, GRID)
+    return demo_diagram(graph, paper)
 }
 
-function drawSections(graph, paper) {
 
-    const sectionDivider = shapes.standard.Link.define('sectionDivider', {
-        attrs: {
-            line: {
-                opacity: "20%",
-                strokeDasharray: "2 5",
-            }}
-        });
-
-    const leftLine = new sectionDivider() // left
-    const rightLine = new sectionDivider() // right
-    const topLine = new sectionDivider() // top
-    const bottomLine = new sectionDivider() // bottom
-
-    leftLine.source({ x: GRID.a, y: 0})
-    leftLine.target({ x: GRID.a, y: PAPERHIEGHT})
-
-    rightLine.source({ x: PAPERWIDTH-GRID.b, y: 0})
-    rightLine.target({ x: PAPERWIDTH-GRID.b, y: PAPERHIEGHT})
-
-    topLine.source({ x: 0, y: GRID.c})
-    topLine.target({ x: PAPERWIDTH, y: GRID.c})
-
-    bottomLine.source({ x: 0, y: PAPERHIEGHT-GRID.d})
-    bottomLine.target({ x: PAPERWIDTH, y: PAPERHIEGHT-GRID.d})
-
-
-    graph.addCells(leftLine, rightLine, topLine, bottomLine)
-}
 
 // pass routing for now
 // function routing() {
